2d694ba4606167eb339729088857ddeaa785d7cf
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-02 15:34:00 -0500

    Solve exercises for chapter 3

---
+++ exercises/chapter_3.sql
@@ -0,0 +1,15 @@
+-- Exercise 3.1
+select title
+from film;
+
+-- Exercise 3.2
+select first_name as "First Name", last_name as "Last Name"
+from actor;
+
+-- Exercise 3.3
+select count(*)
+from inventory;
+
+-- Exercise 3.4
+select actor_id, first_name, last_name, last_update
+from actor;
