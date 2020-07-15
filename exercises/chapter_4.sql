cfe75a9174245922763837c497cab702eaaaf912
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-15 09:44:14 -0500

    Solve exercise 4.3

--- exercises/chapter_4.sql
+++ exercises/chapter_4.sql
@@ -7,3 +7,7 @@ select count(*) as "# customers",
        count(email) as "# customers with email",
        100.0 * count(email) / count(*) as "# with email"
 from customer;
+
+-- Exercise 4.3
+select count(distinct customer_id)
+from payment;
