4aab22281cbd8c00090eef63f88366e76c30ab65
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-15 09:41:15 -0500

    Solve exercise 4.2

--- exercises/chapter_4.sql
+++ exercises/chapter_4.sql
@@ -1,3 +1,9 @@
 -- Exercise 4.1
-select count(customer) as "# customers", count(email) as "# customers with email"
+select count(*) as "# customers", count(email) as "# customers with email"
+from customer;
+
+-- Exercise 4.2
+select count(*) as "# customers",
+       count(email) as "# customers with email",
+       100.0 * count(email) / count(*) as "# with email"
 from customer;
