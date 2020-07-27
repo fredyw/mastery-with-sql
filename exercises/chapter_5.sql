8fd1da5f138ec4a9311315c73ba47e1d1dc4537a
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-26 21:19:12 -0500

    Solve exercise 5.2

--- exercises/chapter_5.sql
+++ exercises/chapter_5.sql
@@ -1,3 +1,7 @@
 -- Exercise 5.1
 select title || ' is ' || coalesce(length || ' minutes', 'unknown') as length_desc
 from film;
+
+-- Exercise 5.2
+select left(title, 3) || repeat('*', length(title) - 3) as "Guess!"
+from film;
