4533accc1295cee0f73bb31fe7ef46160b0c4f03
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-26 22:17:56 -0500

    Solve exercise 5.3

--- exercises/chapter_5.sql
+++ exercises/chapter_5.sql
@@ -5,3 +5,11 @@ from film;
 -- Exercise 5.2
 select left(title, 3) || repeat('*', length(title) - 3) as "Guess!"
 from film;
+
+-- Exercise 5.3
+select round(100.0 * count(*) filter (where rating = 'NC-17') / count(*)) as "% NC-17",
+       round(100.0 * count(*) filter (where rating = 'PG') / count(*)) as "% PG",
+       round(100.0 * count(*) filter (where rating = 'G') / count(*)) as "% G",
+       round(100.0 * count(*) filter (where rating = 'R') / count(*)) as "% R",
+       round(100.0 * count(*) filter (where rating = 'PG-13') / count(*)) as "% PG-13"
+from film;
