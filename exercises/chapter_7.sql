6efb8ae66d753123ac399668d5beba562afb1169
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-31 21:26:37 -0500

    Solve exercise 7.3

--- exercises/chapter_7.sql
+++ exercises/chapter_7.sql
@@ -3,3 +3,26 @@ select distinct first_name, last_name
 from customer
      inner join rental r on customer.customer_id = r.customer_id
 where rental_date::date = (select min(rental_date::date) from rental);
+
+-- Exercise 7.2
+select film_id, title
+from film
+where film_id not in (select film_id from film_actor);
+
+select f.film_id, f.title
+from film f
+     left join film_actor fa on f.film_id = fa.film_id
+where fa.film_id is null;
+
+-- Exercise 7.3
+select c.customer_id, c.first_name, c.last_name
+from customer c
+     inner join rental r
+                on c.customer_id = r.customer_id
+     inner join inventory i on r.inventory_id = i.inventory_id
+where i.film_id = (select i.film_id
+                   from rental r
+                        inner join inventory i on r.inventory_id = i.inventory_id
+                   group by i.film_id
+                   order by count(*), i.film_id
+                   limit 1);
