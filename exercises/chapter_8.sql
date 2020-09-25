4d7a2635177132ef8cd3d08529f122aa156bc190
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-09-25 15:17:44 -0500

    Solve exercise 8.2

--- exercises/chapter_8.sql
+++ exercises/chapter_8.sql
@@ -7,3 +7,18 @@ from (select rental_id,
              over (partition by customer_id order by rental_date desc) as row_number
       from rental) t
 where t.row_number <= 3;
+
+-- Exercise 8.2
+with rent_counts as (
+    select i.film_id,
+           rank() over (order by count(*)) as rank
+    from rental r
+         inner join inventory i on r.inventory_id = i.inventory_id
+    group by i.film_id
+)
+select distinct r.customer_id
+from rental r
+     inner join inventory i on r.inventory_id = i.inventory_id
+where i.film_id in (select film_id
+                    from rent_counts
+                    where rank = 1);
