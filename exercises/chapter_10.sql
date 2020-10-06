854b5f4156fc88666e1f48f08c917281ad39fc09
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-10-06 12:03:46 -0500

    Solve exercise 10.2

--- exercises/chapter_10.sql
+++ exercises/chapter_10.sql
@@ -1,2 +1,11 @@
 -- Exercise 10.1
 create schema beach;
+
+-- Exercise 10.2
+create table beach.customers (
+    first_name text,
+    last_name text,
+    email text,
+    phone text,
+    date date
+);
