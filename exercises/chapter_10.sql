5700fd8d1b74540b3bd069f6f55a693b5f4649cc
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-10-12 19:25:46 -0500

    Solve exercise 10.8

--- exercises/chapter_10.sql
+++ exercises/chapter_10.sql
@@ -47,3 +47,12 @@ create table beach.rentals (
     return_date date,
     primary key (customer_id, equipment_id, rental_date)
 );
+
+-- Exercise 10.8
+create table beach.rentals (
+    customer_id bigint references beach.customers (customer_id) on delete cascade,
+    equipment_id bigint references beach.equipment (equipment_id) on delete cascade,
+    rental_date date,
+    return_date date,
+    primary key (customer_id, equipment_id, rental_date)
+);
