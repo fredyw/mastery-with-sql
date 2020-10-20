876990346630a7c40ae3f75961a5b0bd8ee2a87e
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-10-19 23:57:15 -0500

    Solve exercise 11.2

--- exercises/chapter_11.sql
+++ exercises/chapter_11.sql
@@ -2,3 +2,14 @@
 copy playground.note_tags
 from '/home/fredy/data/github/mastery-with-sql/ch11-note_tags.csv'
 with (format csv, header true);
+
+-- Exercise 11.2
+copy (
+    select u.first_name || ' ' || u.last_name as name, count(*) as note_count
+    from playground.users u
+         inner join playground.notes n on u.email = n.user_email
+    group by name
+    order by note_count desc
+)
+to '/tmp/ch11-2.txt'
+with (format csv, header true);
