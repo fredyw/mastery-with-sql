e739301933ab52d10ccca6160255dfb759df9c11
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-10-28 15:32:22 -0500

    Solve exercise 11.10

--- exercises/chapter_11.sql
+++ exercises/chapter_11.sql
@@ -92,3 +92,11 @@ where amount = 0
 returning *;
 
 rollback;
+
+-- Exercise 11.10
+begin;
+
+delete from language
+where language_id not in (select distinct film.language_id from film);
+
+rollback;
