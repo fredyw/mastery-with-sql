ffe18e0bc418abebffbe8daadc80d4143cc3d5ce
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-10-29 15:37:01 -0500

    Solve exercise 12.2

--- exercises/chapter_12.sql
+++ exercises/chapter_12.sql
@@ -1,6 +1,4 @@
 -- Exercise 12.1
-begin;
-
 create view vw_rental_film as (
     select r.rental_id, f.title, f.length, f.rating
     from rental r
@@ -9,6 +7,11 @@ create view vw_rental_film as (
     order by r.rental_id
  );
 
-select * from vw_rental_film;
-
-rollback;
+-- Exercise 12.2
+select c.customer_id, count(r.rental_id)
+from customer as c
+    left join (rental as r inner join vw_rental_film as rf
+               on r.rental_id = rf.rental_id and rf.rating = 'R')
+               using (customer_id)
+group by c.customer_id
+order by c.customer_id;
